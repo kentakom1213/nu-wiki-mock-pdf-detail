afbd01505bbe21990f9f9cfc8ccb4ba80ad712c2
kentakom1213 <kentakom1213@gmail.com>
2023-03-02 01:17:43 +0900

    [fix] pdfのリンクを見れるものに変更

--- src/lib.rs
+++ src/lib.rs
@@ -17,12 +17,12 @@ const PDF_DATA_JSON: &str = r#"{
         {
             "file_id": 0,
             "name": "微分積分学1",
-            "url": "https://ct.nagoya-u.ac.jp/access/content/attachment/2022_0816130/Assignments/42efe7f1-2c6c-4d5d-b4ee-636737ec18c5/082110424_%E7%AC%AC13%E5%9B%9E%E8%AA%B2%E9%A1%8C_%E4%B8%AD%E6%9D%91%E5%84%AA%E4%BD%9C.pdf"
+            "url": "https://www.nagoya-u.ac.jp/academics/upload_images/5b2f064da9816cd6192d25c3a6d262ae_1.pdf"
         },
         {
             "file_id": 1,
             "name": "線形代数学",
-            "url": "https://ct.nagoya-u.ac.jp/access/content/attachment/2022_0816130/Assignments/42efe7f1-2c6c-4d5d-b4ee-636737ec18c5/082110424_%E7%AC%AC13%E5%9B%9E%E8%AA%B2%E9%A1%8C_%E4%B8%AD%E6%9D%91%E5%84%AA%E4%BD%9C.pdf"
+            "url": "https://www.nagoya-u.ac.jp/academics/upload_images/bosyuuyoko.pdf"
         },
         {
             "file_id": 2,
